fd557a51ffdfd95c059df049e1cc96a594c37361
smin9408 <stmdaks888@naver.com>
2022-04-04 23:30:13 +0900

    BaseActivity 생성 및 상속

--- app/src/main/java/com/example/isitnow/MainActivity.kt
+++ app/src/main/java/com/example/isitnow/MainActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.isitnow.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
 
+    }
 
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
     }
 }
