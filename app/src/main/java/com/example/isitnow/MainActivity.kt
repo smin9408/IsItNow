3fe017e86522f0b3bbcf4c9055d511b3e0fa6076
smin9408 <stmdaks888@naver.com>
2022-04-04 23:26:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/isitnow/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.isitnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.isitnow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+    }
+}
